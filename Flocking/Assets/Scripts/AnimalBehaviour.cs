f8dcbf0f68dcbbdbde4784f3f6bbcde2ac394079
Alex Davidson <alex.davidson@bluewire-technologies.com>
2016-02-04 19:58:51 +0000

    Animals no longer change direction suddenly for closer food
    Target switching only happens if there is no current food target, or if
    the new food target is sufficiently close to the animal's current
    direction of travel.

--- Flocking/Assets/Scripts/AnimalBehaviour.cs
+++ Flocking/Assets/Scripts/AnimalBehaviour.cs
@@ -1,38 +1,79 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Assets.Scripts;
+using Random = UnityEngine.Random;
 
 public class AnimalBehaviour : MonoBehaviour
 {
+    public float MaximumSpeed = 20;
+    public float MaximumAcceleration = 1000;
+ 
     public float HuntingVisionRadius;
-    public float MaximumSpeed;
-    public float MaximumAcceleration;
+    public float HuntingVisionAngleDegrees = 40;
+    public float HuntingTargetSwitchChance = 0.4f;
+    public float HuntingAwarenessSeconds = 0.8f;
+    private float nextHuntingUpdate = 0;
 
     private Transform selfTransform;
     private Rigidbody selfBody;
+
+    private Collider currentConsumable;
+
 
     void Start ()
     {
 	    selfTransform = GetComponent<Transform>();
         selfBody = GetComponent<Rigidbody>();
+        nextHuntingUpdate = Time.time;
 	}
 	
-	void FixedUpdate ()
+    void FixedUpdate ()
     {
-	    var closestConsumable = GetConsumablesInRange().Where(IsVisible).FirstOrDefault();
-        if(closestConsumable == null)
+        // My default behaviour is to gently come to a halt.
+        selfBody.drag = 1;
+
+        if(Time.fixedTime >= nextHuntingUpdate)
         {
-            selfBody.drag = 1;
+            UpdateHunting();
+            nextHuntingUpdate += HuntingAwarenessSeconds;
         }
-        else
+        
+        if(currentConsumable != null)
         {
-            selfBody.drag = 0;
-            ApproachTargetPosition(closestConsumable.transform.position);
+            ApproachTargetPosition(currentConsumable.transform.position);
         }
 	}
 
+    private void UpdateHunting()
+    {
+        if (currentConsumable == null)
+        {
+            // If I have no current consumable target, find the closest one in range.
+            currentConsumable = GetClosestConsumable();
+            return;
+        }
+        var targetSwitchAttempt = Random.value;
+        if(targetSwitchAttempt < HuntingTargetSwitchChance)
+        {
+            // If there's a closer consumable than my current target, maybe switch to it.
+            var closestConsumable = GetClosestConsumable();
+            if(closestConsumable == null || closestConsumable == currentConsumable) return;
+            var bias = Utils.GetVisionCentrednessBias(selfBody.position, selfBody.velocity, closestConsumable.transform.position, HuntingVisionAngleDegrees);
+            if(targetSwitchAttempt < bias * HuntingTargetSwitchChance)
+            {
+                currentConsumable = closestConsumable;
+                return;
+            }
+        }
+    }
+    
+    private Collider GetClosestConsumable()
+    {
+        return GetConsumablesInRange().Where(IsVisible).FirstOrDefault();
+    }
+    
     private void ApproachTargetPosition(Vector3 position)
     {
         var drag = MaximumAcceleration / MaximumSpeed;
@@ -46,25 +87,17 @@ public class AnimalBehaviour : MonoBehaviour
         var force = acceleration - (selfBody.velocity * drag);
 
         selfBody.AddForce(force * Time.fixedDeltaTime);
+        selfBody.drag = 0;
     }
-    private void TryToHalt()
-    {
-        const float damping = 0.8f;
-        selfBody.AddTorque(-selfBody.angularVelocity * Time.fixedDeltaTime * damping);
-        selfBody.AddForce(-selfBody.velocity * Time.fixedDeltaTime * damping);
-    }
-
+    
     private bool IsVisible(Collider target)
     {
-        RaycastHit hit;
-        Physics.Linecast(selfTransform.position, target.transform.position, out hit);
-        return hit.collider == target;
+        return selfTransform.position.CanSee(target);
     }
 
     /// <summary>
     /// Find 'Consumable' objects in hunting vision range, ordered closest to farthest.
     /// </summary>
-    /// <returns></returns>
     private IEnumerable<Collider> GetConsumablesInRange()
     {
 	    var colliders = Physics.OverlapSphere(this.selfTransform.position, HuntingVisionRadius);
--- Flocking/Assets/Scripts/Utils.cs
+++ Flocking/Assets/Scripts/Utils.cs
@@ -11,12 +11,12 @@ namespace Assets.Scripts
     {
         public static float MeasurePrefabRadius(GameObject prefab)
         {
-            if(prefab == null) return 0;
+            if (prefab == null) return 0;
             var radius = MeasureObjectRadius(prefab);
-            if(radius > 0) return radius; // Not actually a prefab?
+            if (radius > 0) return radius; // Not actually a prefab?
 
             // Can apparently only get the prefab's bounds if we instantiate it:
-            var actual = (GameObject) Object.Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            var actual = (GameObject)Object.Instantiate(prefab, Vector3.zero, Quaternion.identity);
             radius = MeasureObjectRadius(actual);
             Object.Destroy(actual);
             return radius;
@@ -24,11 +24,44 @@ namespace Assets.Scripts
 
         public static float MeasureObjectRadius(GameObject obj)
         {
-            if(obj == null) return 0;
-        
+            if (obj == null) return 0;
+
             var bounds = obj.GetComponent<Collider>().bounds;
             return bounds.extents.magnitude;
         }
 
+        /// <summary>
+        /// Get a value in the range 0 - 1 indicating how close to the centre of the body's vision
+        /// the target is, assuming that its direction of motion is the centre of its vision.
+        /// If the body is not moving, it is assumed to be looking directly at the target.
+        /// </summary>
+        public static float GetVisionCentrednessBias(Vector3 selfPosition, Vector3 selfDirection, Vector3 targetPosition, float visionAngleDegrees)
+        {
+            if(selfDirection.magnitude < 0.0001f) return 1;
+
+            // 1 = Directly in front.
+            var bias = Vector3.Dot(selfDirection.normalized,
+                    (targetPosition - selfPosition).normalized);
+        
+            // If visionBoundary is X, we want a result from 0 - 1
+            var visionBoundary = Mathf.Sin(Mathf.Deg2Rad * visionAngleDegrees);
+            if(bias < visionBoundary) return 0;
+
+            return (bias - visionBoundary) / (1 - visionBoundary);
+        }
+
+        /// <summary>
+        /// Returns true if a ray from the eye position strikes the target collider without
+        /// hitting anything else first.
+        /// </summary>
+        /// <remarks>
+        /// Rather crude. Does not consider the extents of the target.
+        /// </remarks>
+        public static bool CanSee(this Vector3 selfEyes, Collider target)
+        {
+            RaycastHit hit;
+            Physics.Linecast(selfEyes, target.transform.position, out hit);
+            return hit.collider == target;
+        }
     }
 }
